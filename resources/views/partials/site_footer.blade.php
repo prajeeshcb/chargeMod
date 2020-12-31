27b500f4d039cdf011897e97570ad8f98e64273c
shilma <shilma@sysboon.com>
2020-12-31 16:23:27 +0530

    bug fixed

--- resources/views/partials/site_footer.blade.php
+++ resources/views/partials/site_footer.blade.php
@@ -1,6 +1,6 @@
 <footer class="site-footer">
   <div class="site-footer-legal">© 2018 <a href="#">BPM POWER PVT. LTD</a></div>
   <div class="site-footer-right">
-    Developed by <a href="http://pixbitsolutions.com/">Pixbit Solutions</a>
+    Developed by <a href="http://pixbitsolutions.com/">chargeMod</a>
   </div>
 </footer>
