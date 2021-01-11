082fef0de8897a467e7072601b661a662e20927f
shilma <shilma@sysboon.com>
2021-01-11 16:26:43 +0530

    user table modifications

--- app/User.php
+++ app/User.php
@@ -1,34 +1,39 @@
 <?php
 
 namespace App;
+
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Database\Eloquent\SoftDeletes;
-class Customers 
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class User extends Authenticatable
 {
     use Notifiable;
-    // use SoftDeletes;
+
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
-    protected $table='customers';
-   // protected $primaryKey='User_ID';
-    protected $fillable = ['User_Type','User_Name','User_Mobile','Username','User_Password','User_Address','User_Pin','User_State','User_District','Status'];
-    // protected $dates = ['deleted_at'];
-
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
 
     /**
      * The attributes that should be hidden for arrays.
      *
      * @var array
      */
-    
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
 
     /**
      * The attributes that should be cast to native types.
      *
      * @var array
      */
-
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
 }
--- database/migrations/2021_01_08_050055_create_customers_table.php
+++ database/migrations/2021_01_08_050055_create_customers_table.php
@@ -14,7 +14,8 @@ class CreateCustomersTable extends Migration
     public function up()
     {
         Schema::create('customers', function (Blueprint $table) {
-            $table->increments('User_ID');
+            $table->increments('Id');
+            $table->integer('User_ID');
             $table->integer('User_Type');
             $table->string('User_Name');
             $table->integer('User_Mobile');
